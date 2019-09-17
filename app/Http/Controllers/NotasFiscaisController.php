daef31eb0edcb57c82ff73ce96863a4a18794fa6
Fabio Pontes <faabiopontes@gmail.com>
2019-09-16 21:04:32 -0300

    FEA Laravel 5.8 com Tela e Controller Inicial XPTO

---
+++ app/Http/Controllers/NotasFiscaisController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NotasFiscaisController extends Controller
+{
+    public function verificarNovas(Request $e) {
+        dd("verificarNovas");
+    }
+}
